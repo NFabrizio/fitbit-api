78395c8301d0aedc0715d504cd604c281ab4e152
NFabrizio <nick.fabrizio@pearson.com>
2017-05-30 08:21:27 -0600

    Adds routes for Fitbit authentication

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Hapi = require ('hapi');
 const Fitbit = require('fitbit-node');
 const Config = require('./config/config');
 
-const client = new Fitbit(Config.clientId, Config.clientSecret);
+const client = new Fitbit(Config.fitbit_creds.clientId, Config.fitbit_creds.clientSecret);
 const redirect_uri = 'http://localhost:8080/fitbit_oauth_callback';
-const scope = 'acitivity profile';
+const scope = 'activity profile';
 
 const server = new Hapi.Server();
 server.connection({ port: 8080 });
@@ -15,6 +15,26 @@ server.route([
     path: "/",
     handler: (requst, reply) => {
       reply('Hello world from Hapi');
+    }
+  },
+  {
+    method: "GET",
+    path: "/fitbit",
+    handler: (requst, reply) => {
+      reply().redirect(client.getAuthorizeUrl(scope, redirect_uri));
+    }
+  },
+  {
+    method: "GET",
+    path: "/fitbit_oauth_callback",
+    handler: (request, reply) => {
+      client.getAccessToken(request.query.code, redirect_uri)
+      .then((result) => {
+        client.get('/profile.json', result.access_token)
+        .then((profile) => {
+          reply(profile)
+        });
+      });
     }
   }
 ]);
